5297ef06bc472f18a1c07fefc36e43a3b45bf451
yxxnhx <tjdbsgk514@hanmail.net>
2023-03-18 16:50:06 +0900

    검색 결과 페이지 생성
    검색 결과를 삼항연산자를 이용하여 내용이 있을 시와 없을 시를 분리하여 구현하였습니다

---
+++ pages/search/[index].tsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import Seo from "../../components/Seo";
+import { MdManageSearch } from "react-icons/md";
+import MovieBox from "../../components/MovieBox";
+
+interface Movie {
+  id: number;
+  title: string;
+  poster_path: string;
+}
+
+interface Props {
+  results: Movie[];
+  params: string;
+}
+
+export default function Home({ results, params }: Props) {
+  return (
+    <div>
+      <Seo title={params} />
+      <h1>Results of &lsquo;{params}&rsquo;</h1>
+      {results.length ? (
+        <MovieBox results={results} />
+      ) : (
+        <div>
+          <MdManageSearch />
+          <p>No result found</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export async function getServerSideProps(context: {
+  query: { query: string };
+}): Promise<{ props: Props }> {
+  const params = context.query.query as string;
+  const { results }: { results: Movie[] } = await axios
+    .get(`http://localhost:3000/api/search?query=${params}&language=ko-KR`)
+    .then((res) => res.data);
+
+  results.map(
+    (item) =>
+      (item.poster_path = `https://image.tmdb.org/t/p/w500${item.poster_path}`),
+  );
+
+  return { props: { results, params } };
+}
