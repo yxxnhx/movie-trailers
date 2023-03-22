ab32fdd05c5d595080d736f9bb7fdc4640a74347
yxxnhx <tjdbsgk514@hanmail.net>
2023-03-23 08:02:58 +0900

    _app 컴포넌트 react query 설정 추가
    react query를 사용할 수 있도록 설정을 추가해주었습니다

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,64 @@
+import React from "react";
+import Head from "next/head";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+import { NextComponentType } from "next";
+import { AppContext, AppInitialProps, AppProps } from "next/app";
+import { Hydrate } from "react-query/hydration";
+import { ReactQueryDevtools } from "react-query/devtools";
+
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
-}
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
+const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
+  Component,
+  pageProps,
+}) => {
+  const queryClientRef = React.useRef<QueryClient>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+
+  return (
+    <QueryClientProvider client={queryClientRef.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Head>
+          <title>Movie Trailer</title>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+        <main>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </main>
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
+
+MyApp.getInitialProps = async ({
+  Component,
+  ctx,
+}: AppContext): Promise<AppInitialProps> => {
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return { pageProps };
+};
 export default MyApp;
